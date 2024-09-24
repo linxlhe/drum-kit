5323f4802b7f21dd04757fbac4a475ea22f48c1a
Lin He <xlhe@cs.washington.edu>
2024-09-23 17:18:51 -0700

    change button color when clicked

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 // make sure all the buttons have event listener
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-        var audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
+        this.style.color = "white";
+        
     });
 }
+
+
+// var audio = new Audio('./sounds/tom-1.mp3');
+//         audio.play();
